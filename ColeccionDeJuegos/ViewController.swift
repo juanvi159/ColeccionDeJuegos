8ff39a75d41d32f4e89f0d2c80fe5726eff50b4a
Juan Obando <juan.obando.a@tecsup.edu.pe>
2021-05-18 11:19:53 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Juan Vicente Obando Aquice on 5/18/21.
+//  Copyright © 2021 Juan Vicente Obando Aquice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
